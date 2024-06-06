f5bb11241576f021e619ad2c2428754f13634529
mac-rodasridar <richard_rodas01@hotmail.com>
2024-06-06 16:40:39 -0500

    fix hdration , quitando injects

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -4,13 +4,11 @@ import { appConfig } from './app.config';
 import { APP_BASE_HREF } from '@angular/common';
 import { ClientResolver } from './services/client.resolver';
 import { REQUEST_SUBDOMAIN, SUBDOMAIN } from '../../src/subdomain.token';
-import { provideClientHydration } from '@angular/platform-browser';
  
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    provideClientHydration(),
-    ClientResolver,
+    // ClientResolver,
     {
       provide: APP_ID,
       useValue: 'serverApp',
